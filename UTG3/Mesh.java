9b187abb88172c3b4abdaa5110c226584bc71f0d
Alex <121258962+AI3X123@users.noreply.github.com>
2024-05-17 13:58:31 -0500

    Add files via upload

---
+++ UTG3/Mesh.java
@@ -0,0 +1,9 @@
+package pack;
+
+public class Mesh {
+
+	public Mesh() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ UTG3/Polygon2D.java
@@ -0,0 +1,9 @@
+package pack;
+
+public class Polygon2D {
+
+	public Polygon2D() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
