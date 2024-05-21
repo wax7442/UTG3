d311b575ab25a4355cc402fe34515e859a19121f
RealHellmit08 <carterhaas2@gmail.com>
2024-05-21 13:47:35 -0500

    Update Physics.java

--- UTG3/src/pack/Physics.java
+++ UTG3/src/pack/Physics.java
@@ -1,15 +1,6 @@
 public class Physics
 {
   static final double GRAVITY = 9.80665;
-  
-  static double getGravity()
-  {
-    return GRAVITY;
-  }
 
-  public double collide(Polygon3D polygon)
-  {
-  
-  }
   
 }
