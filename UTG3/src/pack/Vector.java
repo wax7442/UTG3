c7fa98f30af6c55a881870d4509e35c34323037b
RealHellmit08 <carterhaas2@gmail.com>
2024-05-21 13:51:08 -0500

    Create Vector.java

---
+++ UTG3/src/pack/Vector.java
@@ -0,0 +1,18 @@
+public class Vector
+  {
+    private double force, direction;
+
+    public Vector()
+    {
+      force = Physics.GRAVITY;
+      direction = 3*Math.PI/4;
+    }
+
+    public Vector(forc, dir)
+    {
+      force = forc;
+      direction = dir;
+    }
+
+    
+  }
