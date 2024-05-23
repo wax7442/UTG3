6b29ab5a4cb1c7992cb9c762c3baee8e5cf6ebd4
RealHellmit08 <carterhaas2@gmail.com>
2024-05-23 13:42:17 -0500

    Update Physics.java

--- UTG3/src/pack/Physics.java
+++ UTG3/src/pack/Physics.java
@@ -1,6 +1,14 @@
-public class Physics
+/* Gives a general outline of how vectors can interact
+*  update 5/23/2024
+*  authors Carter Haas, Kyle Slavik
+*/
+public abstract class Physics
 {
   static final double GRAVITY = 9.80665;
-  static final Vector GVECTOR = new Vector (0,0,GRAVITY*-1);
+  static final AccVector GVECTOR = new AccVector (0,0,GRAVITY*-1);
   
+  public Vector applyAccel(Vector vector,AccVector accVector)
+  {
+  	return new Vector(vector.getForceX+accVector.getForceX,vector.getForceY+accVector.getForceY,vector.getForceZ+accVector.getForceZ);
+  }
 }
