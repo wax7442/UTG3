9c384667e1ab3f89da4524e4d87d0d7de3e83bb0
wax7442 <wilkinsonbrody2@gmail.com>
2024-05-23 14:43:56 -0500

    Update PointGenerator.java

--- UTG3/src/pack/PointGenerator.java
+++ UTG3/src/pack/PointGenerator.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 import java.io.File;
 
-private double x;
-private double y;
-private double z;
+private String pointX, pointY, pointZ;
+private double x, y, z;
+private int lastIndex;
 private File file;
 private String vertex;
 private ArrayList <Point3D> vertexList;
@@ -22,8 +22,20 @@ public class PointGenerator
         {
           if(input.nextLine().substringOf(0,2).equals("v "))
           {
-          vertex = input.nextLine().substringOf(3);
-          
+            vertex = input.nextLine().substringOf(3);
+              
+            lastIndex = vertex.indexOf(" ");
+            pointX = vertex.substringOf("0, lastIndex+1);
+            x = Double.valueOf(pointX);
+            vertex = vertex.substringOf(lastIndex+1);
+            lastIndex = vertex.indexOf(" ");
+            pointY = vertex.substringOf("0, lastIndex+1);
+            y = Double.valueOf(pointY);
+            vertex = vertex.substringOf(lastIndex+1);
+            pointZ = Double.valueOf(vertex);
+            vertexList.add(new Point3D (x, y, z);
           }
         }
-          
+    }
+  }
+//No IDEA if this works even close to how I want it to, but I'm about done for the day...
