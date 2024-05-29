44b9b1d16ec4339439c54339676a932445a258f1
Alex <121258962+AI3X123@users.noreply.github.com>
2024-05-29 00:23:08 -0500

    Update Test.java

--- UTG3/src/pack/pack/Test.java
+++ UTG3/src/pack/pack/Test.java
@@ -26,12 +26,61 @@ public class Test { // basic class for testing out the engine. Very basic.
     	scene.add(
     		new Mesh(
     			new Polygon3D[] {
-    				new Polygon3D(new Point3D[] {new Point3D(300, 200, 200), new Point3D(300, 400, 200), new Point3D(600, 400, 200), new Point3D(600, 200, 200)}, Color.red), 
-    				new Polygon3D(new Point3D[] {new Point3D(300, 200, 200), new Point3D(300, 400, 200), new Point3D(300, 400, 500), new Point3D(300, 200, 500)}, Color.orange), 
-    				new Polygon3D(new Point3D[] {new Point3D(600, 400, 200), new Point3D(600, 200, 200), new Point3D(600, 200, 500), new Point3D(600, 400, 500)}, Color.white), 
-    				new Polygon3D(new Point3D[] {new Point3D(300, 200, 200), new Point3D(600, 200, 200), new Point3D(600, 200, 500), new Point3D(300, 200, 500)}, Color.pink), 
-    				new Polygon3D(new Point3D[] {new Point3D(300, 400, 200), new Point3D(600, 400, 200), new Point3D(600, 400, 500), new Point3D(300, 400, 500)}, Color.yellow), 
-    				new Polygon3D(new Point3D[] {new Point3D(300, 400, 500), new Point3D(300, 200, 500), new Point3D(600, 200, 500), new Point3D(600, 400, 500)}, Color.green)
+//    				new Polygon3D(new Point3D[] {new Point3D(300, 200, 200), new Point3D(300, 400, 200), new Point3D(600, 400, 200), new Point3D(600, 200, 200)}, Color.red), 
+//    				new Polygon3D(new Point3D[] {new Point3D(300, 200, 200), new Point3D(300, 400, 200), new Point3D(300, 400, 500), new Point3D(300, 200, 500)}, Color.orange), 
+////    				new Polygon3D(new Point3D[] {new Point3D(600, 400, 200), new Point3D(600, 200, 200), new Point3D(600, 200, 500), new Point3D(600, 400, 500)}, Color.white), 
+//    				new Polygon3D(new Point3D[] {new Point3D(300, 200, 200), new Point3D(600, 200, 200), new Point3D(600, 200, 500), new Point3D(300, 200, 500)}, Color.pink), 
+////    				new Polygon3D(new Point3D[] {new Point3D(300, 400, 200), new Point3D(600, 400, 200), new Point3D(600, 400, 500), new Point3D(300, 400, 500)}, Color.yellow), 
+////    				new Polygon3D(new Point3D[] {new Point3D(300, 400, 500), new Point3D(300, 200, 500), new Point3D(600, 200, 500), new Point3D(600, 400, 500)}, Color.green)
+    					new Polygon3D(new Point3D[] {
+    						    // Front face of the house
+    						    new Point3D(100, 100, 100), new Point3D(100, 300, 100), new Point3D(300, 300, 100), new Point3D(300, 100, 100)
+    						}, Color.gray),
+    						new Polygon3D(new Point3D[] {
+    						    // Left face of the house
+    						    new Point3D(100, 100, 100), new Point3D(100, 100, 300), new Point3D(100, 300, 300), new Point3D(100, 300, 100)
+    						}, Color.blue),
+    						new Polygon3D(new Point3D[] {
+    						    // Right face of the house
+    						    new Point3D(300, 100, 100), new Point3D(300, 300, 100), new Point3D(300, 300, 300), new Point3D(300, 100, 300)
+    						}, Color.green),
+    						new Polygon3D(new Point3D[] {
+    						    // Back face of the house
+    						    new Point3D(100, 100, 300), new Point3D(300, 100, 300), new Point3D(300, 300, 300), new Point3D(100, 300, 300)
+    						}, Color.yellow),
+    						new Polygon3D(new Point3D[] {
+    						    // Bottom face of the house
+    						    new Point3D(100, 100, 100), new Point3D(300, 100, 100), new Point3D(300, 100, 300), new Point3D(100, 100, 300)
+    						}, Color.cyan),
+    						new Polygon3D(new Point3D[] {
+    						    // Top face of the house
+    						    new Point3D(100, 300, 100), new Point3D(300, 300, 100), new Point3D(300, 300, 300), new Point3D(100, 300, 300)
+    						}, Color.magenta),
+
+    						// Roof of the house (pyramid)
+    						new Polygon3D(new Point3D[] {
+    						    // Front-left roof
+    						    new Point3D(100, 300, 100), new Point3D(300, 300, 100), new Point3D(200, 400, 200)
+    						}, Color.red),
+    						new Polygon3D(new Point3D[] {
+    						    // Back-left roof
+    						    new Point3D(100, 300, 100), new Point3D(100, 300, 300), new Point3D(200, 400, 200)
+    						}, Color.orange),
+    						new Polygon3D(new Point3D[] {
+    						    // Front-right roof
+    						    new Point3D(300, 300, 100), new Point3D(300, 300, 300), new Point3D(200, 400, 200)
+    						}, Color.pink),
+    						new Polygon3D(new Point3D[] {
+    						    // Back-right roof
+    						    new Point3D(100, 300, 300), new Point3D(300, 300, 300), new Point3D(200, 400, 200)
+    						}, Color.MAGENTA),
+
+    						// Door of the house (rectangle)
+    						new Polygon3D(new Point3D[] {
+    						    // Door on the front face
+    						    new Point3D(180, 100, 100), new Point3D(220, 100, 100), new Point3D(220, 200, 100), new Point3D(180, 200, 100)
+    						}, Color.blue)
+
     			}
     		)
     	);
@@ -39,7 +88,7 @@ public class Test { // basic class for testing out the engine. Very basic.
 		ArrayList<Polygon2D> image = scene.getActiveCamera().getImage(scene);
 		System.out.println(image.get(0));
 		
-		scene.getActiveCamera().printSpaceAhead();
+//		scene.getActiveCamera().printSpaceAhead();
 		
 		Display display = new Display();
 		display.init(800, 600, "UTG3");	//Why don't we put the code of this method in the constructor? -Loff
